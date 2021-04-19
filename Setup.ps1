d47f432f92615870895fb3534491269a726201b4
williamyoung2196 <69459942+williamyoung2196@users.noreply.github.com>
2021-04-19 11:20:59 +0100

    Initial Upload

---
+++ Setup.ps1
@@ -0,0 +1,6 @@
+﻿Set-ExecutionPolicy remotesigned
+function downloadbuilder {
+Invoke-WebRequest -Uri "https://github.com/williamyoung2196/PurpleJelly-DeviceSetup/archive/master.zip" -OutFile "C:\PurpleJelly\PJDeviceSetup.zip"
+Expand-Archive C:\PurpleJelly\PJDeviceSetup.zip -DestinationPath C:\PurpleJelly\
+}
+downloadbuilder
